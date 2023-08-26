7c8954456e49144341333289bc51f1d3b203b197
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-08-26 23:41:35 +0300

    fix: update validation

--- middlewares/validations/user.js
+++ middlewares/validations/user.js
@@ -1,21 +1,10 @@
 import { celebrate, Joi } from 'celebrate';
 
-export const loginValidation = celebrate({
+export const patchMeValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi
-      .string()
-      .required()
-      .email()
-      .min(2)
-      .max(30),
-    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().email(),
   }),
 });
 
-export const registerValidation = celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-  }),
-});
+export default patchMeValidation;
--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getMe, patchMe } from '../controllers/user.js';
+import patchMeValidation from '../middlewares/validations/user.js';
 
 const userRouter = Router();
 
 userRouter.get('/me', getMe);
-userRouter.patch('/me', patchMe);
+userRouter.patch('/me', patchMeValidation, patchMe);
 
 export default userRouter;
