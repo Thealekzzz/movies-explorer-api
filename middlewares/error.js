8231d182e4298680a5e6f4dadb011d3158e011b0
Alex Kuznetsov <kznv.alex@yandex.ru>
2023-08-26 23:21:25 +0300

    refactor: remove log

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,8 +1,6 @@
 export default (err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
-  console.log(err);
-
   res.status(statusCode).send({
     message: statusCode === 500
       ? 'Серверная ошибка'
